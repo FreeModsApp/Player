c2091592a0829e77197a57f3e5300e48df5489ef
Legend Akshay <88607836+FreeModsApp@users.noreply.github.com>
2022-02-19 17:25:18 +0530

    Update __main__.py

--- WebStreamer/__main__.py
+++ WebStreamer/__main__.py
@@ -1,59 +1,61 @@
 # This file is a part of TG-FileStreamBot
-# Coding : Jyothis Jayanth [@EverythingSuckz]
+# Coding: @EverythingSuckz & @AbirHasan2005
 
+import os
+import sys
+import glob
 import asyncio
 import logging
+import importlib
+from pathlib import Path
+from pyrogram import idle
+from .bot import StreamBot
 from .vars import Var
 from aiohttp import web
-from pyrogram import idle
-from WebStreamer import utils
-from WebStreamer import bot_info
-from WebStreamer.server import web_server
-from WebStreamer.bot.clients import initialize_clients
+from .server import web_server
 
-
-logging.basicConfig(
-    level=logging.INFO, format="%(asctime)s - %(name)s - %(levelname)s - %(message)s"
-)
-logging.getLogger("pyrogram").setLevel(logging.ERROR)
-logging.getLogger("aiohttp").setLevel(logging.ERROR)
-logging.getLogger("aiohttp.web").setLevel(logging.ERROR)
+ppath = "WebStreamer/bot/plugins/*.py"
+files = glob.glob(ppath)
 
 loop = asyncio.get_event_loop()
 
 
 async def start_services():
-    print("----------------------------- DONE -----------------------------")
-    print()
-    print(
-        "----------------------------- Initializing Clients -----------------------------"
-    )
-    await initialize_clients()
-    print("----------------------------- DONE -----------------------------")
-    if Var.ON_HEROKU:
-        print("------------------ Starting Keep Alive Service ------------------")
-        print()
-        asyncio.create_task(utils.ping_server())
-    print("-------------------- Initalizing Web Server --------------------")
+    print('\n')
+    print('------------------- Initalizing Telegram Bot -------------------')
+    await StreamBot.start()
+    print('\n')
+    print('---------------------- DONE ----------------------')
+    print('\n')
+    print('------------------- Importing -------------------')
+    for name in files:
+        with open(name) as a:
+            patt = Path(a.name)
+            plugin_name = patt.stem.replace(".py", "")
+            plugins_dir = Path(f"WebStreamer/bot/plugins/{plugin_name}.py")
+            import_path = ".plugins.{}".format(plugin_name)
+            spec = importlib.util.spec_from_file_location(import_path, plugins_dir)
+            load = importlib.util.module_from_spec(spec)
+            spec.loader.exec_module(load)
+            sys.modules["WebStreamer.bot.plugins." + plugin_name] = load
+            print("Imported => " + plugin_name)
+    print('\n')
+    print('------------------- Initalizing Web Server -------------------')
     app = web.AppRunner(await web_server())
     await app.setup()
-    bind_address = "0.0.0.0" if Var.ON_HEROKU else Var.BIND_ADDRESS
+    bind_address = "0.0.0.0" if Var.ON_HEROKU else Var.FQDN
     await web.TCPSite(app, bind_address, Var.PORT).start()
-    print("----------------------------- DONE -----------------------------")
-    print()
-    print("----------------------- Service Started -----------------------")
-    print("                        bot =>> {}".format(bot_info.first_name))
-    if bot_info.dc_id:
-        print("                        DC ID =>> {}".format(str(bot_info.dc_id)))
-    print("                        server ip =>> {}".format(bind_address, Var.PORT))
+    print('\n')
+    print('----------------------- Service Started -----------------------')
+    print('                        bot =>> {}'.format((await StreamBot.get_me()).first_name))
+    print('                        server ip =>> {}:{}'.format(bind_address, Var.PORT))
     if Var.ON_HEROKU:
-        print("                        app running on =>> {}".format(Var.FQDN))
-    print("---------------------------------------------------------------")
+        print('                        app runnng on =>> {}'.format(Var.FQDN))
+    print('---------------------------------------------------------------')
     await idle()
 
-
-if __name__ == "__main__":
+if __name__ == '__main__':
     try:
         loop.run_until_complete(start_services())
     except KeyboardInterrupt:
-        logging.info("----------------------- Service Stopped -----------------------")
+        print('----------------------- Service Stopped -----------------------')
